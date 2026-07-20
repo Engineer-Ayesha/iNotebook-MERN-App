7de2659f09636792c655cb8bc0b0a87e44aae342
Engineer-Ayesha <engineerayeshakhalid@gmail.com>
2026-07-20 04:56:30 -0700

    Restore fetchUser middleware

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = "secretUsername";
+const fetchUser = (req, res, next) => {
+  const token = req.header("authToken");
+  if (!token) {
+    res.status(401).send({ error: "Please enter a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, jwtSecret);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Please enter a valid token" });
+  }
+};
+module.exports = fetchUser;
